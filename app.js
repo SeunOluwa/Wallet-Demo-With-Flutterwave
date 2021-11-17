c2f87a05134f4da34a83c80f306ea36a8f727326
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-11-17 17:03:58 +0100

    login function

--- app.js
+++ app.js
@@ -60,8 +60,36 @@ app.post('/register', async (req, res) => {
 });
 
 // Login
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     // our login logic goes here
+    try {
+        // get user input
+        const { email, password } = req.body;
+
+        // validate user input
+        if (!(email && password)) {
+            res.status(400).send('All input is required');
+        }
+        // validate if user exist in the database
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                { expiresIn: '2h' }
+            );
+            // save user token
+            user.token = token;
+
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send('Invalid Credentials');
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 module.exports = app;
