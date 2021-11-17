87933fcb38d4054a718539c546987dca12860ee2
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-11-17 14:58:40 +0100

    user schema

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+// importing user context
+const User = require('./model/User');
+
 const app = express();
 
 app.use(express.json());
 
 // Logic here
+// Register
+app.post('/register', (req, res) => {
+    // our register logic goes here
+});
+
+// Login
+app.post('/login', (req, res) => {
+    // our login logic goes here
+});
 
 module.exports = app;
