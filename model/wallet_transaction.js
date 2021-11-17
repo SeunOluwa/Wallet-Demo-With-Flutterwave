4fdba5084253f34d98f450954c003f9a7de6804a
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-11-17 19:44:36 +0100

    wallet transaction schema

---
+++ model/wallet_transaction.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const walletTransactionSchema = new mongoose.Schema({
+    amount: { type: Number, default: 0 },
+
+    // even though user can be implied from wallet, let us
+    // double save it for security
+    userId: {
+        type: String,
+        ref: 'users',
+        required: true
+    },
+
+    isInFlow: { type: Boolean },
+
+    paymentMethod: { type: String, default: 'flutterwave' },
+
+    currency: {
+        type: String,
+        required: [true, 'payment status is required'],
+        enum: ['successful', 'pending', 'failed']
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('WalletTransaction', walletTransactionSchema);
