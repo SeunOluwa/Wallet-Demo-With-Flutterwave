e2df2a0a3ab394358f7d9c87cc514d7be857a4d1
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-11-18 07:28:46 +0100

    controllers and routes

--- app.js
+++ app.js
@@ -1,95 +1,14 @@
 require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-// importing user context
-const User = require('./model/User');
+const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
 
-// Logic here
-// Register
-app.post('/register', async (req, res) => {
-    // our register logic goes here
-    try {
-        // get user input
-        const { first_name, last_name, email, password } = req.body;
-
-        // validate user input
-        if (!(email && password && first_name && last_name)) {
-            res.status(400).send('All input is required');
-        }
-
-        // check if user already exist
-        // validate if user already exist in the database
-        const oldUser = await User.findOne({ email });
-
-        if (oldUser) {
-            return res.status(409).send('User Already Exist. Please Login');
-        }
-
-        // encrypt user password
-        encryptedPassword = await bcrypt.hash(password, 10);
-
-        // create user in the database
-        const user = await User.create({
-            first_name,
-            last_name,
-            email: email.toLowerCase(), // sanitize: convert email to lowercase
-            password: encryptedPassword  
-        });
-
-        // create token
-        const token = jwt.sign(
-            { user_id: user._id, email },
-            process.env.TOKEN_KEY,
-            { expiresIn: '2h' }
-        );
-        // save user token
-        user.token = token;
-
-        // return new user
-        res.status(201).json(user);
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-// Login
-app.post('/login', async (req, res) => {
-    // our login logic goes here
-    try {
-        // get user input
-        const { email, password } = req.body;
-
-        // validate user input
-        if (!(email && password)) {
-            res.status(400).send('All input is required');
-        }
-        // validate if user exist in the database
-        const user = await User.findOne({ email });
-
-        if (user && (await bcrypt.compare(password, user.password))) {
-            // create token
-            const token = jwt.sign(
-                { user_id: user._id, email },
-                process.env.TOKEN_KEY,
-                { expiresIn: '2h' }
-            );
-            // save user token
-            user.token = token;
-
-            // user
-            res.status(200).json(user);
-        }
-        res.status(400).send('Invalid Credentials');
-    } catch (err) {
-        console.log(err);
-    }
-});
+// middlewares
+app.use('/users', userRoutes);
 
 module.exports = app;
---
+++ controllers/userController.js
@@ -0,0 +1,90 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+require('dotenv').config();
+
+// importing user context
+const User = require('../model/User');
+
+const signup = async (req, res) => {
+    // our register logic goes here
+    try {
+        // get user input
+        const { first_name, last_name, email, password } = req.body;
+
+        // validate user input
+        if (!(email && password && first_name && last_name)) {
+            res.status(400).send('All input is required');
+        }
+
+        // check if user already exist
+        // validate if user already exist in the database
+        const oldUser = await User.findOne({ email });
+
+        if (oldUser) {
+            return res.status(409).send('User Already Exist. Please Login');
+        }
+
+        // encrypt user password
+        encryptedPassword = await bcrypt.hash(password, 10);
+
+        // create user in the database
+        const user = await User.create({
+            first_name,
+            last_name,
+            email: email.toLowerCase(), // sanitize: convert email to lowercase
+            password: encryptedPassword  
+        });
+
+        // create token
+        const token = jwt.sign(
+            { user_id: user._id, email },
+            process.env.TOKEN_KEY,
+            { expiresIn: '2h' }
+        );
+        // save user token
+        user.token = token;
+
+        // return new user
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const signin = async (req, res) => {
+    // our login logic goes here
+    try {
+        // get user input
+        const { email, password } = req.body;
+
+        // validate user input
+        if (!(email && password)) {
+            res.status(400).send('All input is required');
+        }
+        // validate if user exist in the database
+        const user = await User.findOne({ email });
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // create token
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                { expiresIn: '2h' }
+            );
+            // save user token
+            user.token = token;
+
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send('Invalid Credentials');
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = {
+    signup,
+    signin
+}
